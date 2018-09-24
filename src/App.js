a687e88a49c0046fc0d9595307519d777f6eb886
rachelho <rachho1998@gmail.com>
2018-09-24 00:45:41 -0600

    Added axios library to easily query GitHub API. Created UI for user cards and input.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from "./Form";
+
+const Card = props => {
+  console.log(props);
+  return (
+    <div style={{ margin: '1em' }}>
+      <img alt="avatar" style={{ width: '70px' }} src={props.avatar_url} />
+      <div>
+        <div style={{ fontWeight: 'bold' }}>{props.name}</div>
+        <div>{props.blog}</div>
+        <div>{props.bio}</div>
+      </div>
+    </div>
+  )
+}
+
+const CardList = props => {
+  return <div>{props.cards.map(card => <Card {...card} />)}</div>
+}
 
 class App extends Component {
+  state = {
+    cards: []
+  }
+  
+  addNewCard = cardInfo => {
+    this.setState(prevState => ({
+      cards: prevState.cards.concat(cardInfo)
+    }))
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">GitHub-React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <div>
+            <Form onSubmit={this.addNewCard} />
+            <CardList cards={this.state.cards} />
+        </div>
         </p>
       </div>
     );
